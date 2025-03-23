61b66704eee4821fb093ad273d45be96e1df1e52
yadavsaurabh9923 <yadavsaurabh9923@gmail.com>
2025-03-23 19:47:49 +0530

    Fixed Routing Issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import { CssVarsProvider } from '@mui/joy/styles'
-import BasicGrid from './components/Homepage'
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssVarsProvider } from '@mui/joy/styles';
+import BasicGrid from './components/Homepage';
+import React from 'react';
 import TermsAndConditions from './pages/TermsAndConditions';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import RefundsAndCancellations from './pages/RefundsAndCancellations';
-import ShippingDeliveryPolicy from './pages/ShippingAndDelivery'
-import ContactUs from './pages/ContactUs'
+import ShippingDeliveryPolicy from './pages/ShippingAndDelivery';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <CssVarsProvider>
-      <Routes>
-        <Route path="/" element={<BasicGrid />} />
-        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/refunds-cancellations" element={<RefundsAndCancellations />} />
-        <Route path="/shipping-and-delivery" element={<ShippingDeliveryPolicy />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/logo" element={<ContactUs />} />
-      </Routes>
+      <Router>  {/* ✅ Wrap everything inside BrowserRouter */}
+        <Routes>
+          <Route path="/" element={<BasicGrid />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/refunds-cancellations" element={<RefundsAndCancellations />} />
+          <Route path="/shipping-and-delivery" element={<ShippingDeliveryPolicy />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </Router>
     </CssVarsProvider>
-  )
+  );
 }
 
-export default App
+export default App;
